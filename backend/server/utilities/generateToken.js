7a30958669d9945c8140cca290b5f7f502ff866a
Adrian Florczyk <aflorczyk51@gmail.com>
2023-11-16 11:28:24 -0500

    firstName, lastName working

--- backend/server/utilities/generateToken.js
+++ backend/server/utilities/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 const dotenv = require('dotenv');
 dotenv.config();
 
-const generateAccessToken = (userId, email, username, password ) => {
+const generateAccessToken = (userId, firstName, lastName, email, username, password ) => {
     return jwt.sign({id: userId, firstName, lastName, email, username, password},process.env.ACCESS_TOKEN_SECRET,{
         expiresIn:'1m'
     })
