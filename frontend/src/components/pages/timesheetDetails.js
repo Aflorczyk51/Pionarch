ab0bb0a857ded90a4cc92cd97affc6607d17a923
Adrian Florczyk <aflorczyk51@gmail.com>
2023-12-13 09:51:12 -0500

    Timesheet import fix

--- frontend/src/components/pages/timesheetDetails.js
+++ frontend/src/components/pages/timesheetDetails.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
 import { useParams, useNavigate } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
